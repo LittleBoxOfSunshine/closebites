4bcb63267fdcfd845efd3f7510a2db7052f08663
branmcf25 <branmcf25@gmail.com>
2017-04-27 02:50:26 +0000

    Added some routes

--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,31 @@
 <?php
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+
+$app->get('/', function($request,$response,$args) {
+    return "Welcome to Slim 3.0 based API";
 });
+
+$app->post('/login', function($request,$response,$args) {
+    return "POST /login";
+});
+
+$app->post('/register', function($request,$response,$args) {
+    return "POST /register";
+});
+
+$app->post('/favorite', function($request,$response,$args) {
+    return "POST /favorite";
+});
+
+$app->post('/unfavorite', function($request,$response,$args) {
+    return "POST /unfavorite";
+});
+
+$app->get('/saveFilter', function($request,$response,$args) {
+    return "GET /saveFilter";
+});
+
+
+
