6c9c44e9eae97142b009b14a4309a719fd335f63
vagrant <vagrant@vagrant.vm>
2017-05-02 06:48:10 +0000

    vendor/create endpoint; still needs polishing

--- src/routes.php
+++ src/routes.php
@@ -1,25 +1,25 @@
 <?php
 
+//set up global session to save user_id and vendor_id
+session_start();
+
 function getDB() {
-  $dbhost="localhost";
-  $dbuser="root";
-  $dbpass="pass"; # CHANGE LATER
-  $dbname="closebites";
-  $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
-  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  return $dbh;
+   $dbhost="localhost";
+   $dbuser="root";
+   $dbpass="pass"; # CHANGE LATER
+   $dbname="closebites";
+   $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   return $dbh;
 }
 
 // Routes
 $app->get('/', function ($request, $response, $args) {
     // Sample log message
-
     // Render index view
     return "hello";
 });
-
 $app->group('/api', function() use ($app) {
-
     $app->get('/temp', function($request, $response, $args){
         return $response->withJson(            [
             ['id'=>1,'name'=>"McDonald's",'description'=>"Deal 1 text",'type'=>'food'],
@@ -30,17 +30,40 @@ $app->group('/api', function() use ($app) {
             ['id'=>6,'name'=>'Jamba Juice','description'=>'Deal 6 text','type'=>'drink'],
         ]);
     });
-
     $app->group('/User', function() use ($app) {
-
+        $app->get('/exists', function($request,$response,$args) {
+          $body = $request->getParsedBody();
+          $email = $body['email'];
+          $password = $body['password'];
+          $type = $body['email'];
+          if($type == 'consumer') {
+            $query = "SELECT email FROM user WHERE user.email = '$email' AND user.password = '$password'";
+            $db = getDB();
+            $result = $db->query($query);
+            if($result) {
+              return "200";
+            } else {
+              return "400";
+            }
+          }
+          else {
+            if($body['email'] == 'vendor') {
+              $query = "SELECT email FROM user WHERE user.email = '$email' AND user.password = '$password'";
+              $db = getDB();
+              $result = $db->query($query);
+              if($result) {
+                return "200";
+              } else {
+                return "400";
+              }
+            }
+          }
+        });
         $app->post('/login', function($request,$response,$args) {
-
             /**
              *  THIS IS A HARDCODED TEST RESPONSE FOR FRONTEND TESTING
              */
-
             $body = $request->getParsedBody();
-
             if($body['email'] != 'vendor') {
                 return $response->withJson([
                     'id'=> 0,
@@ -82,27 +105,40 @@ $app->group('/api', function() use ($app) {
                             'id' => 0,
                             'start' => '2016/05/15 15:0',
                             'end' => '2016/05/15 19:00',
-                            'repeat' => '0010000'
+                            '_repeat' => '0010000'
                         ],
                         [
                             'name' => 'Thirsty Thursday',
                             'id' => 1,
                             'start' => '2016/05/15 17:30',
                             'end' => '2016/05/15 19:30',
-                            'repeat' => '0000100'
+                            '_repeat' => '0000100'
                         ]
                     ]
                 ]);
             }
         });
-
         $app->post('/register', function($request,$response,$args) {
             /**
              *  THIS IS A HARDCODED TEST RESPONSE FOR FRONTEND TESTING
              */
-
-            $body = $request->getParsedBody();
-
+             $body = $request->getParsedBody();
+             $email = $body['email'];
+             $password = $body['password'];
+             $options = [
+               'cost' => 11,
+               'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+             ];
+             $hash = password_hash($password, PASSWORD_BCRYPT, $options);
+            //  return $hash;
+             $query = "INSERT INTO user (user_id, email, password) VALUES (6, '$email', '$hash')";
+             $db = getDB();
+             $db->query($query);
+            //  if($result) {
+            //    return "200";
+            //  } else {
+            //    return "400";
+            //  }
             if($body['accountType'] == 'consumer') {
                 return $response->withJson([
                     'id'=> 0,
@@ -144,14 +180,14 @@ $app->group('/api', function() use ($app) {
                             'id' => 0,
                             'start' => '2016/05/15 15:0',
                             'end' => '2016/05/15 19:00',
-                            'repeat' => '0010000'
+                            '_repeat' => '0010000'
                         ],
                         [
                             'name' => 'Thirsty Thursday',
                             'id' => 1,
                             'start' => '2016/05/15 17:30',
                             'end' => '2016/05/15 19:30',
-                            'repeat' => '0000100'
+                            '_repeat' => '0000100'
                         ]
                     ]
                 ]);
@@ -160,46 +196,71 @@ $app->group('/api', function() use ($app) {
                 return $response->withStatus(422);
             }
         });
-
-        $app->post('/favorite', function($request,$response,$args) {
-            return "POST /favorite";
+        $app->get('/favorite', function($request,$response,$args) {
+          $dbhost="localhost";
+          $dbuser="root";
+          $dbpass="pass";
+          $dbname="closebites";
+          $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+          $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $query="select * from deal";
+          $result = $dbh->query($query);
+          while($row = $result->fetch(PDO::FETCH_ASSOC)){
+              $data[] = $row;
+          }
+          return json_encode($data);
         });
-
         $app->post('/unfavorite', function($request,$response,$args) {
             return "POST /unfavorite";
         });
-
         $app->get('/saveFilter', function($request,$response,$args) {
-            return "GET /saveFilter";
+            return $response->withJson([
+                'id'=> 0,
+                'name' => 'Steak n shake',
+                'accountType' => 'vendor',
+                'address' => '123 fake street',
+                'calendar' => [
+                    [
+                        'name' => 'Drinks Tuesday',
+                        'id' => 0,
+                        'start' => '2016/05/15 15:0',
+                        'end' => '2016/05/15 19:00',
+                        '_repeat' => '0010000'
+                    ],
+                    [
+                        'name' => 'Shakes Saturday',
+                        'id' => 1,
+                        'start' => '2016/05/15 17:30',
+                        'end' => '2016/05/15 19:30',
+                        '_repeat' => '0000100'
+                    ]
+                ]
+            ]);
+            // return "GET /saveFilter";
         });
-
     });
-
 });
-
 $app->post('/login', function($request,$response,$args) {
-
     $body = $request->getParsedBody();
     $email = $body['email'];
     $password = $body['password'];
-    if($body['email'] == 'consumer') {
-
+    if($body['email'] == 'matt') {
+      return "200";
+    } else {
+      return "400";
     }
     $query = "SELECT email FROM user WHERE user.email = '$email' AND user.password = '$password'";
-
     $db = getDB();
     $result = $db->query($query);
-
+    // $row = $result->fetch(PDO::FETCH_ASSOC)
     if($result) {
       return "200";
     } else {
       return "400";
     }
-
     return $body['email'];
 });
-
-$app->get('/find', function($request,$response,$args) {
+$app->get('/findAll', function($request,$response,$args) {
     //$connection = $this->get("db");
     $dbhost="localhost";
     $dbuser="root";
@@ -207,14 +268,66 @@ $app->get('/find', function($request,$response,$args) {
     $dbname="closebites";
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     $query="select * from deal";
     $result = $dbh->query($query);
-
     while($row = $result->fetch(PDO::FETCH_ASSOC)){
         $data[] = $row;
     }
     return json_encode($data);
-
     //return "Welcome to Slim 3.0 based API";
 });
+    
+
+$app->group('/Vendor', function() use ($app) {
+
+   // vendor/create route
+   $app->post('/create/{vendor_id}', function($request,$response,$args) {
+
+      //pull out arg into variable
+      $vendor_id = $args['vendor_id'];
+      
+      //run the connection to the database again 
+      $dbh = getDB();
+
+      //parse request
+      $body = $request->getParsedBody();
+      
+      //insert deal query 
+      $sql = $dbh->prepare("insert into deal (vendor_id,title,start_time,end_time,_repeat,description,norm_price,discount,type) values (:vendor_id,:title,:start_time,:end_time,:_repeat,:description,:norm_price,:discount,:type)"); 
+      $sql->bindParam('title',$title);
+      $sql->bindParam('start_time',$start_time);
+      $sql->bindParam('end_time',$end_time);
+      $sql->bindParam('_repeat',$_repeat);
+      $sql->bindParam('description',$description);
+      $sql->bindParam('norm_price',$norm_price);
+      $sql->bindParam('discount',$discount);
+      $sql->bindParam('type',$type);
+      $sql->bindParam('vendor_id',$vendor_id);
+
+      //set variables for insert deal query
+      $title = $body['title'];
+      $start_time = $body['start_time'];
+      $end_time = $body['end_time'];
+      $_repeat = $body['_repeat'];
+      $description = $body['description'];
+      $norm_price = $body['norm_price'];
+      $discount = $body['discount'];
+      $type = $body['type'];
+      $sql->execute(); //run insert deal 
+      $deal_id = $dbh->lastInsertId();
+
+      /*
+      $last_inserted = $dbh->query("SELECT LAST_INSERT_ID()");
+      $deal_id = $last_inserted->fetchColumn();
+      */
+
+      //create category query
+      $sql = $dbh->prepare("insert into category (title) values ('$type')");
+      $sql->execute(); //run insert category
+
+      //return deal_id
+      return $deal_id;
+
+   });//end vendor/create route
+
+});//end Vendor group
