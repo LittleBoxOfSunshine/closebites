499af5651fa0dadb0d34d1716b3fac01e89b2536
Matthew Lee <matthew.lee711@gmail.com>
2017-05-03 01:08:30 -0500

    swapped exists from post to get

--- app/routes/api.php
+++ app/routes/api.php
@@ -34,7 +34,7 @@ $app->group('/api', function() use ($app) {
 
     $app->group('/User', function() use ($app) {
 
-        $app->post('/exists', function($request,$response,$args) {
+        $app->get('/exists', function($request,$response,$args) {
 
           $body = $request->getParsedBody();
           $email = $body['email'];
