788d26a6b6397cc0b0a0469d8aec1d2870feafa6
burrowssean <sburrows@smu.edu>
2017-05-03 20:53:45 +0000

    formatting

--- app/routes/api.php
+++ app/routes/api.php
@@ -46,83 +46,83 @@ $app->group('/api', function() use ($app) {
          //insert deal query
          $sql = $dbh->prepare("insert into deal (user_id,title,start_date,end_date,repeats,description,norm_price,discount_price) values (:user_id,:title,:start_date,:end_date,:repeats,:description,:norm_price,:discount_price)");
          $sql->bindParam('title',$title);
-       $sql->bindParam('start_date',$start_date);
-      $sql->bindParam('end_date',$end_date);
-      $sql->bindParam('repeats',$repeats);
-      $sql->bindParam('description',$description);
-      $sql->bindParam('norm_price',$norm_price);
-      $sql->bindParam('discount_price',$discount_price);
-      //$sql->bindParam('type',$type);
-      $sql->bindParam('user_id',$user_id);
-      //$sql->bindParam('category_id',$category_id);
+         $sql->bindParam('start_date',$start_date);
+         $sql->bindParam('end_date',$end_date);
+         $sql->bindParam('repeats',$repeats);
+         $sql->bindParam('description',$description);
+         $sql->bindParam('norm_price',$norm_price);
+         $sql->bindParam('discount_price',$discount_price);
+         //$sql->bindParam('type',$type);
+         $sql->bindParam('user_id',$user_id);
+         //$sql->bindParam('category_id',$category_id);
 
-      //set variables for insert deal query
-      $user_id = $_SESSION['user_id'];
-      $title = $body['title'];
-      $start_date = $body['start_date'];
-      $end_date = $body['end_date'];
-      $repeats = $body['repeats'];
-      $description = $body['description'];
-      $norm_price = $body['norm_price'];
-      $discount_price = $body['discount_price'];
-      //$type = $body['type'];
-      //$category_id = $body['category_id'];
-      $sql->execute(); //run insert deal
-      $deal_id = $dbh->lastInsertId();
+         //set variables for insert deal query
+         $user_id = $_SESSION['user_id'];
+         $title = $body['title'];
+         $start_date = $body['start_date'];
+         $end_date = $body['end_date'];
+         $repeats = $body['repeats'];
+         $description = $body['description'];
+         $norm_price = $body['norm_price'];
+         $discount_price = $body['discount_price'];
+         //$type = $body['type'];
+         //$category_id = $body['category_id'];
+         $sql->execute(); //run insert deal
+         $deal_id = $dbh->lastInsertId();
 
-      //return deal_id
-      return $deal_id;
+         //return deal_id
+         return $deal_id;
 
-   });//end vendor/create route
+      });//end vendor/create route
 
-   // vendor/update route
-   $app->post('/update/{deal_id}', function($request,$response,$args) {
+      // vendor/update route
+      $app->post('/update/{deal_id}', function($request,$response,$args) {
 
-      //run the connection to the database again
-      $dbh = getDB();
+         //run the connection to the database again
+         $dbh = getDB();
 
-      //parse request
-      $body = $request->getParsedBody();
+         //parse request
+         $body = $request->getParsedBody();
 
-      //update deal query
-      $sql = $dbh->prepare("update deal set user_id=:user_id,category_id=:category_id,title=:title,start_date=:start_date,end_date=:end_date,repeats=:repeats,description=:description,norm_price=:norm_price,discount_price=:discount_price,type=:type where :deal_id=deal.deal_id");
-      $sql->bindParam('title',$title);
-      $sql->bindParam('start_date',$start_date);
-      $sql->bindParam('end_date',$end_date);
-      $sql->bindParam('repeats',$repeats);
-      $sql->bindParam('description',$description);
-      $sql->bindParam('norm_price',$norm_price);
-      $sql->bindParam('discount_price',$discount_price);
-      $sql->bindParam('type',$type);
-      $sql->bindParam('user_id',$user_id);
-      $sql->bindParam('category_id',$category_id);
-      $sql->bindParam('deal_id',$deal_id);
+         //update deal query
+         $sql = $dbh->prepare("update deal set user_id=:user_id,category_id=:category_id,title=:title,start_date=:start_date,end_date=:end_date,repeats=:repeats,description=:description,norm_price=:norm_price,discount_price=:discount_price,type=:type where :deal_id=deal.deal_id");
+         $sql->bindParam('title',$title);
+         $sql->bindParam('start_date',$start_date);
+         $sql->bindParam('end_date',$end_date);
+         $sql->bindParam('repeats',$repeats);
+         $sql->bindParam('description',$description);
+         $sql->bindParam('norm_price',$norm_price);
+         $sql->bindParam('discount_price',$discount_price);
+         $sql->bindParam('type',$type);
+         $sql->bindParam('user_id',$user_id);
+         $sql->bindParam('category_id',$category_id);
+         $sql->bindParam('deal_id',$deal_id);
 
-      //set variables for update deal query
-      $user_id = $_SESSION['user_id'];
-      $title = $body['title'];
-      $start_date = $body['start_date'];
-      $end_date = $body['end_date'];
-      $repeats = $body['repeats'];
-      $description = $body['description'];
-      $norm_price = $body['norm_price'];
-      $discount_price = $body['discount_price'];
-      $type = $body['type'];
-      $category_id = $body['category_id'];
-      $deal_id = $args['deal_id'];
-      $sql->execute(); //run insert deal
+         //set variables for update deal query
+         $user_id = $_SESSION['user_id'];
+         $title = $body['title'];
+         $start_date = $body['start_date'];
+         $end_date = $body['end_date'];
+         $repeats = $body['repeats'];
+         $description = $body['description'];
+         $norm_price = $body['norm_price'];
+         $discount_price = $body['discount_price'];
+         $type = $body['type'];
+         $category_id = $body['category_id'];
+         $deal_id = $args['deal_id'];
+         $sql->execute(); //run insert deal
 
-   });//end vendor/create route
+      });//end vendor/create route
 
-});//end Vendor group
+   });//end Vendor group
 
-//deal group
-$app->group('/Deal', function() use ($app) {
+   //deal group
+   $app->group('/Deal', function() use ($app) {
 
-   //get feedback route
-   $app->get('/getFeedback/{deal_id}', function($request,$response,$args) {
-      //pull out deal_id
-      $deal_id = $args['deal_id'];
+      //get feedback route
+      $app->get('/getFeedback/{deal_id}', function($request,$response,$args) {
+         //pull out deal_id
+         $deal_id = $args['deal_id'];
 
       //run the connection to the database again
       $dbh = getDB();
@@ -136,36 +136,37 @@ $app->group('/Deal', function() use ($app) {
       $results = $sql->fetchAll();
       return json_encode($results);
 
-   });//end getFeedback
+      });//end getFeedback
 
-   //post feedback route
-   $app->post('/feedback/{deal_id}', function($request,$response,$args) {
+      //post feedback route
+      $app->post('/feedback/{deal_id}', function($request,$response,$args) {
 
-      //run the connection to the database again
-      $dbh = getDB();
+         //run the connection to the database again
+         $dbh = getDB();
 
-      //parse request
-      $body = $request->getParsedBody();
+         //parse request
+         $body = $request->getParsedBody();
 
-      //insert feedback query
-      $sql = $dbh->prepare("insert into comment (user_id,deal_id,comment) values (:user_id,:deal_id,:comment)");
+         //insert feedback query
+         $sql = $dbh->prepare("insert into comment (user_id,deal_id,comment) values (:user_id,:deal_id,:comment)");
 
-      $sql->bindParam('user_id',$user_id);
-      $sql->bindParam('deal_id',$deal_id);
-      $sql->bindParam('comment',$comment);
+         $sql->bindParam('user_id',$user_id);
+         $sql->bindParam('deal_id',$deal_id);
+         $sql->bindParam('comment',$comment);
 
-      //set variables for insert feedback query
-      $user_id = $_SESSION['user_id'];
-      $comment = $body['comment'];
-      $deal_id = $args['deal_id'];
-      $arr = array($deal_id,$comment,$user_id);
+         //set variables for insert feedback query
+         $user_id = $_SESSION['user_id'];
+         $comment = $body['comment'];
+         $deal_id = $args['deal_id'];
+         $arr = array($deal_id,$comment,$user_id);
 
-      $sql->execute();
+         $sql->execute();
 
-   });//end insert feedback route
+      });//end insert feedback route
 
-});//end Vendor group
-    $app->group('/User', function() use ($app) {
+   });//end Vendor group
+
+   $app->group('/User', function() use ($app) {
 
         $app->post('/exists', function($request,$response,$args) {
 
