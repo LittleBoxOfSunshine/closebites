d6cf8b7605f669cb166eb2c77911c7162d1c4756
cahenk <cahenk95@gmail.com>
2017-05-04 17:30:48 -0500

    fixed if organization

--- app/routes/api.php
+++ app/routes/api.php
@@ -157,7 +157,7 @@ $app->group('/api', function() use ($app) {
                }
                else if($body['isVendor'] && $_SESSION['user_id'] != $row['user_id'])
                    continue;
-               else if(isset($body['zip']) && $body['zip'] != -1 && strpos($row['address'], ((string)$body['zip'])) !== false)
+               if(isset($body['zip']) && $body['zip'] != -1 && strpos($row['address'], ((string)$body['zip'])) !== false)
                    continue;
                $cuisineQuery = "SELECT `type` FROM vendor WHERE user_id=".$row['user_id'];
                $result = $dbh->query($cuisineQuery);
